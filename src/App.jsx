ec86a66da858140c3f92e38c491a39e5b9d9f13c
陈黎明 <275298711@qq.com>
2020-08-07 17:57:01 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -14,3 +14,5 @@ function App(props) {
 }
 
 export default App;
+
+
--- src/layouts/AdminLayout/index.jsx
+++ src/layouts/AdminLayout/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {renderRoutes} from 'react-router-config'
+
 const Index = function (props){
     const { route } = props
+    console.log(99999999999,renderRoutes(route.routes))
     return <div>
         <h1>AdminLayout</h1>
         {renderRoutes(route.routes)}
